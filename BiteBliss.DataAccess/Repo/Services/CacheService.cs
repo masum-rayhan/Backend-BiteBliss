5a4c04ffb8a6060c4d9d58055ecdc2dd2460a5d4
Masum Rayhan <73586966+masum-rayhan@users.noreply.github.com>
2023-09-15 07:40:34 +0600

    feat: Add the implementation of ICacheService to the CacheService repo

---
+++ BiteBliss.DataAccess/Repo/Services/CacheService.cs
@@ -0,0 +1,44 @@
+﻿using BiteBliss.DataAccess.Repo.IRepo.Services;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace BiteBliss.DataAccess.Repo.Services;
+
+public class CacheService : ICacheService
+{
+    private IDatabase _cacheDb;
+    public CacheService()
+    {
+        var redis = ConnectionMultiplexer.Connect("localhost:6379");
+        _cacheDb = redis.GetDatabase();
+    }
+
+    public T GetData<T>(string key)
+    {
+        var value = _cacheDb.StringGet(key);
+        if(!string.IsNullOrEmpty(value))
+            return JsonSerializer.Deserialize<T>(value);
+        return default;
+    }
+
+    public object RemoveData(string key)
+    {
+        var _exist = _cacheDb.KeyExists(key);
+
+        if (_exist)
+            return _cacheDb.KeyDelete(key);
+
+        return false;
+    }
+
+    public bool SetData<T>(string key, T data, DateTimeOffset expirationTime)
+    {
+        var expiryTime = expirationTime.DateTime.Subtract(DateTime.Now);
+        return _cacheDb.StringSet(key, JsonSerializer.Serialize(data), expiryTime);
+    }
+}
