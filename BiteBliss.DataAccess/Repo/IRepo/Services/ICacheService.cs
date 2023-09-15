7fa3fd3ce292c05735f4631cc519af8c81339161
Masum Rayhan <73586966+masum-rayhan@users.noreply.github.com>
2023-09-15 07:30:35 +0600

    feat: Add ICacheService at the IRepo

---
+++ BiteBliss.DataAccess/Repo/IRepo/Services/ICacheService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiteBliss.DataAccess.Repo.IRepo.Services;
+
+public interface ICacheService
+{
+    T GetData<T>(string key);
+    bool SetData<T>(string key, T data, DateTimeOffset expirationTime);
+    object RemoveData(string key);
+}
